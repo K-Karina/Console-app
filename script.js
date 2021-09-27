bcf811bfb566ba0b18c05636774c820ff4ccbb4a
K-Karina <dom.karuci@gmail.com>
2021-09-27 22:54:49 +0300

    feat: automate questions with films using cycles

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-'use strict';
+"use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-const lastWatchedMovie1 = prompt('Один из последних просмотренных фильмов?', '');
-const lastWatchedMovieAssess1 = +prompt('На сколько оцените его?', '');
-const lastWatchedMovie2 = prompt('Один из последних просмотренных фильмов?', '');
-const lastWatchedMovieAssess2 = +prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[lastWatchedMovie1] = lastWatchedMovieAssess1;
-personalMovieDB.movies[lastWatchedMovie2] = lastWatchedMovieAssess2;
-
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+  const lastWatchedMovie1 = prompt(
+      "Один из последних просмотренных фильмов?",
+      ""
+    ),
+    lastWatchedMovieAssess1 = +prompt("На сколько оцените его?", "");
+  personalMovieDB.movies[lastWatchedMovie1] = lastWatchedMovieAssess1;
+}
